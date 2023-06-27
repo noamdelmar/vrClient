5fffed67053ffacfa9e628a5fdb09154a2914166
noamdelmar <noam.delmar@gmail.com>
2023-06-27 13:54:41 +0300

    all tags functions

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,40 +7,53 @@ export default function Home() {
         const createTag = async () => {
             try {
                 const newTag = {
-                    name: 'שטח',
-                    description: 'תוצרים בנושא שטח'
+                    name: 'שמיקולאס',
+                    description: 'מיקולאס'
                 }
-                const res = await httpCommon.post('createTag', newTag)
+                const res = await httpCommon.post('tags/create', newTag)
+                console.log(res)
             } catch (err) {
                 console.error('error creating tag:', err);
             }
         }
         const getTag = async () => {
             try {
-                const idQuery = {
+                const data = {
                     id: 1
                 }
-                const query = new URLSearchParams(idQuery).toString();
-                const res = await httpCommon.get(`/getTags?${query}`)
+                const res = await httpCommon.get('getTags', { data })
                 console.log(res.data)
             } catch (err) {
                 console.error('error creating tag:', err);
             }
         }
         const updateTag = async () => {
+            const data = {
+                id: 1,
+                name: 'description',
+                value: 'אוליבר'
+            }
             try {
-                const data = {
-                    id: 1,
-                    name: 'name',
-                    value: 'אוליבר'
-                }
                 const res = await httpCommon.put('updateTag', data);
                 console.log(res);
             } catch (err) {
                 console.error('error creating tag:', err);
             }
         }
-        updateTag()
+
+        const deleteTag = async () => {
+            const data = {
+                id: 9
+            }
+            try {
+                const res = await httpCommon.delete('deleteTag', { data });
+                console.log(res);
+            } catch (err) {
+                console.error('error deleting tag:', err);
+            }
+        }
+        // deleteTag()
+        // updateTag()
         // createTag()
         // getTag()
     })
